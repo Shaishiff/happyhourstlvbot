246b71fbf826b4f97329e0b336a37b4e836006f8
Shai Shiff <shai@crossrider.com>
2016-05-26 16:36:10 +0300

    - test conversation

--- server.js
+++ server.js
@@ -113,6 +113,34 @@ controller.hears(["menu","תפריט"], 'message_received', function(bot, messag
   View.showMainMenu(bot, message);
 });
 
+controller.hears(['pizzatime'], 'message_recieved', function(bot,message) {
+    askFlavor = function(response, convo) {
+      convo.ask('What flavor of pizza do you want?', function(response, convo) {
+        if (response) console.log(JSON.stringify(response));
+        convo.say('Awesome.');
+        askSize(response, convo);
+        convo.next();
+      });
+    }
+    askSize = function(response, convo) {
+      convo.ask('What size do you want?', function(response, convo) {
+        if (response) console.log(JSON.stringify(response));
+        convo.say('Ok.')
+        askWhereDeliver(response, convo);
+        convo.next();
+      });
+    }
+    askWhereDeliver = function(response, convo) {
+      convo.ask('So where do you want it delivered?', function(response, convo) {
+        if (response) console.log(JSON.stringify(response));
+        convo.say('Ok! Good bye.');
+        convo.next();
+      });
+    }
+
+    bot.startConversation(message, askFlavor);
+});
+
 // Similar string.
 controller.hears(["(.*)"], 'message_received', function(bot, message) {
     if(message.text && message.text.length > 0) {
