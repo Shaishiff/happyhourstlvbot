efa124cfc8d30a98866635e317b871b93a439e51
Shai Shiff <shai@crossrider.com>
2016-08-23 16:10:53 +0300

    - removed unused code

--- code/postBackHelper.js
+++ code/postBackHelper.js
@@ -3,13 +3,6 @@
 var Controller = require('./controller');
 var postBackHelper = {};
 
-postBackHelper.handlePostBack = function(bot, message, payload) {
-	if(payload.indexOf("-") === -1 ) payload += "-";
-	var postBackId = payload.split("-")[0];
-	var postBackData = payload.split("-")[1];
-	if (typeof Controller[postBackId] === "function") {
-		Controller[postBackId](bot, message, postBackData);
-	}
-}
+
 
 module.exports = postBackHelper;
